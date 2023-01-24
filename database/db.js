12cc6293bd8d700b7523d39941a4b131818e27e6
MangDTech <mangtech24@gmail.com>
2023-01-24 18:08:20 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'isi dengan url database mongodb';
+var url = 'mongodb+srv://MangDTech:@Pisces24@cluster0.ijakb.mongodb.net/?retryWrites=true&w=majority';
 try {
-if(url == 'isi dengan url database mongodb') throw console.log(color('Configurasi database, var url belum diisi','red'));
+if(url == 'mongodb+srv://MangDTech:@Pisces24@cluster0.ijakb.mongodb.net/?retryWrites=true&w=majority') throw console.log(color('Configurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
